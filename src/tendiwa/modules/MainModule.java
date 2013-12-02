51a4353a81aa802460056ae07def672d6ffad589
Suseika <wlasowegor@gmail.com>
2013-12-02 15:24:23 +0600

    Basics of inventory UI; refactoring of items system; started refactoring apparel system.

--- src/tendiwa/modules/MainModule.java
+++ src/tendiwa/modules/MainModule.java
@@ -3,11 +3,11 @@ package tendiwa.modules;
 import tendiwa.core.*;
 import tendiwa.drawing.*;
 import tendiwa.resources.CharacterTypes;
+import tendiwa.resources.ItemTypes;
 
 import java.awt.*;
 
-import static tendiwa.core.DSL.*;
-import static tendiwa.core.World.create;
+import static tendiwa.core.DSL.canvas;
 
 public class MainModule extends Module implements WorldProvider {
 
@@ -36,7 +36,11 @@ public static void main(String[] args) {
 @Override
 public World createWorld() {
 	World world = World.create(new SuseikaWorld(), 400, 300);
-	world.setPlayerCharacter(new PlayerCharacter(world.getDefaultPlane(), 200, 150, "Suseika", CharacterTypes.human, "warrior"));
+	PlayerCharacter playerCharacter = new PlayerCharacter(world.getDefaultPlane(), 200, 150, "Suseika", CharacterTypes.human, "warrior");
+	world.setPlayerCharacter(playerCharacter);
+	playerCharacter.getItem(new UniqueItem(ItemTypes.short_bow));
+	playerCharacter.getItem(new UniqueItem(ItemTypes.iron_armor));
+	playerCharacter.getItem(new UniqueItem(ItemTypes.iron_helm));
 	return world;
 }
 }

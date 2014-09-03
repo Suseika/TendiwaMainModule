1db5df9c4e651706f81003b796f97c06314262b1
Suseika <wlasowegor@gmail.com>
2014-09-04 01:46:29 +0600

    Implement getting direction to segment from rectangle

--- src/main/java/org/tendiwa/modules/mainModule/CoastlinePixmap.java
+++ src/main/java/org/tendiwa/modules/mainModule/CoastlinePixmap.java
@@ -1,10 +1,10 @@
 package org.tendiwa.modules.mainModule;
 
+import com.google.common.collect.ImmutableList;
 import org.jgrapht.UndirectedGraph;
 import org.tendiwa.core.Location;
 import org.tendiwa.core.Tendiwa;
 import org.tendiwa.core.World;
-import org.tendiwa.demos.Demos;
 import org.tendiwa.drawing.TestCanvas;
 import org.tendiwa.drawing.extensions.DrawingWorld;
 import org.tendiwa.geometry.CellSegment;
@@ -13,15 +13,22 @@ import org.tendiwa.geometry.Rectangle;
 import org.tendiwa.geometry.Segment2D;
 import org.tendiwa.geometry.extensions.CachedCellSet;
 import org.tendiwa.groovy.Registry;
-import org.tendiwa.settlements.RoadsPlanarGraphModel;
 import org.tendiwa.settlements.RectangleWithNeighbors;
+import org.tendiwa.settlements.buildings.BuildingsTouchingStreets;
+import org.tendiwa.settlements.buildings.LotFacadeAssigner;
+import org.tendiwa.settlements.buildings.UrbanPlanner;
+import org.tendiwa.settlements.streets.Namer;
 import org.tendiwa.settlements.utils.RoadRejector;
+import org.tendiwa.settlements.utils.StreetsDetector;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
 import java.util.function.Consumer;
 
+/**
+ */
 public class CoastlinePixmap implements Runnable {
 	private final CoastlineGeometry geometry;
 
@@ -70,13 +77,18 @@ public class CoastlinePixmap implements Runnable {
 			CoastlineCityGeometry city
 			: geometry.cities
 			) {
-			UndirectedGraph<Point2D, Segment2D> graphToDraw = RoadRejector.rejectPartOfNetworksBorders(
+			UndirectedGraph<Point2D, Segment2D> actualRoadGraph = RoadRejector.rejectPartOfNetworksBorders(
 				city.roadsPlanarGraphModel.getFullRoadGraph(),
 				city.roadsPlanarGraphModel,
 				0.5,
 				new Random(123445634)
 			);
-			graphToDraw.edgeSet().forEach(drawRoad);
+			Set<ImmutableList<Point2D>> streets = StreetsDetector.detectStreets(actualRoadGraph);
+			BuildingsTouchingStreets b2s = new BuildingsTouchingStreets(streets, 3.3);
+			Namer<List<Point2D>> streetNamer = (line) -> "Улица Говна";
+//			LotFacadeAssigner facadeAssigner = (lot) -> b2s.getStreetsForLot(lot).iterator().next();
+//			new UrbanPlanner(world.getDefaultPlane(), 3.3).addAvailableArchitecture();
+			actualRoadGraph.edgeSet().forEach(drawRoad);
 			city.roadsPlanarGraphModel.getNetworks().stream()
 				.flatMap(cell -> cell.network().edgeSet().stream())
 				.forEach(drawRoad);

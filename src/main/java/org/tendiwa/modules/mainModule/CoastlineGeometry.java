50ebb4e9f223152faa670b7f1b65063a0e28ff81
Suseika <wlasowegor@gmail.com>
2015-03-08 19:20:11 +0500

    Fixed a bug with split events in straight skeleton occurring in impossible situations; fixed InnerTree construction (enclosed cycles weren't being added)

--- src/main/java/org/tendiwa/modules/mainModule/CoastlineGeometry.java
+++ src/main/java/org/tendiwa/modules/mainModule/CoastlineGeometry.java
@@ -57,8 +57,8 @@ public class CoastlineGeometry implements Runnable {
 		int minDistanceBetweenCityCenters = maxCityRadius * 3;
 		int minDistanceFromCoastToCityCenter = 20;
 		SimpleNoiseSource noise = (x, y) -> Noise.noise(
-			((double) x + 900) / 100,
-			((double) y + 200) / 100,
+			((double) x + 13900) / 100,
+			((double) y + 13200) / 100,
 			7
 		);
 		water = (x, y) -> noise.noise(x, y) <= 110;
@@ -221,11 +221,8 @@ public class CoastlineGeometry implements Runnable {
 	}
 
 	private void createCanvas() {
-		//		canvas = new MagnifierCanvas(10, 60, 301, 600, 600);
-//		canvas = new MagnifierCanvas(6, 620, 1241, 800, 800);
-//		canvas = new MagnifierCanvas(8, 1468, 1165, 800, 800);
-//		canvas = new MagnifierCanvas(5, 1449, 1148, 800, 800);
-		canvas = new TestCanvas(1, worldSize.width, worldSize.height);
+		canvas = new MagnifierCanvas(3, 1180, 570, 800, 800);
+//		canvas = new TestCanvas(1, worldSize.width, worldSize.height);
 //		canvas = new NullCanvas();
 	}
 
